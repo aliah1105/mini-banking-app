6f4b76bb2675a47dbcf62f7078ecb3bec9b43c94
aliah1105 <aliahadi.f1105@gmail.com>
2024-11-07 18:33:35 +0330

    use xml and json headers for sending request base on that

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	ZipCode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	ZipCode string `json:"zip_code" xml:"zipCode"`
 }
 
 func main() {
@@ -27,6 +28,12 @@ func getAllCustomers(writer http.ResponseWriter, request *http.Request) {
 		{"Ali", "mashhad", "1234"},
 		{"Amin", "mashhad", "12345"},
 	}
-	writer.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(writer).Encode(customers)
+	if request.Header.Get("Content-Type") == "application/xml" {
+		writer.Header().Set("Content-Type", "application/xml")
+		xml.NewEncoder(writer).Encode(customers)
+	} else {
+		writer.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(writer).Encode(customers)
+	}
+
 }
